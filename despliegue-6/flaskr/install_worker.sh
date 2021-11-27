8c31a018bb70f80d4dafc8b3e7bd9131c022f4f2
Anderson Luis Caceres Padilla <al.caceres@uniandes.edu.co>
2021-11-27 11:41:44 -0500

    Modificaciones

--- despliegue-6/flaskr/install_worker.sh
+++ despliegue-6/flaskr/install_worker.sh
@@ -1,15 +1,21 @@
 #!bin/bash
+# sudo su
 apt update
 docker image rm -f $(docker image ls -q)
+add-apt-repository -y ppa:deadsnakes/ppa
+apt update
+apt -y remove python3.6
+apt -y install python3.8 build-essential ffmpeg postgresql-server-dev-all libpython3.8-dev python3-pip
+rm -rf /usr/bin/python
+ln -s /usr/bin/python3 /usr/bin/python
 mkdir /home/app
 cd /home/app
 git clone https://github.com/anderson0365/miso-softwarecloud-gr-17.git
 cd miso-softwarecloud-gr-17/despliegue-6/flaskr
-apt -y install build-base ffmpeg postgresql-dev bash python3 python3-dev py3-pip
-ln -s /usr/bin/python3 /usr/bin/python
-pip install --no-cache-dir -r requirements.txt --ignore-installed six
+pip install --no-cache-dir -r requirements.txt
 pip install --no-cache-dir pydub gunicorn boto3
-mv wsgi.py ../
-cp config.yml ../
+cp wsgi.py config.yml ../
 chmod 777 start-celery.sh start-flask.sh wait-for-it.sh
-./start-celery.sh
+cd ..
+celery -A  flaskr.tasks.tasks worker -B -l info
+# exit
